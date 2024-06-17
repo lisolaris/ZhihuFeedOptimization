3246d763a996a612cb9201f9f6af69a58ac7c410
ligeek <ligeek@outlook.com>
2024-06-17 08:55:20 +0800

    Add color for console log, fix spelling mistake

--- ZhihuFeedOptimization.user.js
+++ ZhihuFeedOptimization.user.js
@@ -29,15 +29,16 @@
 
         const fetchPromises = [];
         for (let card of cards) {
-            let cardItem = card.querySelector("div.ContentItem.AnswerItem");
-            if (!cardItem) cardItem = card.querySelector("div.ContentItem.ArticleItem");
+            // 每个内容卡片都具有class: "ContentItem ArticleItem"或"ContentItem AnswerItem"
+            let cardItem = card.querySelector("div.ContentItem");
+            // if (!cardItem) cardItem = card.querySelector("div.ContentItem.ArticleItem");
             // console.log("card: " + card.textContent)
 
             let extraInfo = JSON.parse(cardItem.getAttribute("data-za-extra-module"));
             let userId = extraInfo.card.content.author_member_hash_id;
 
             // 此用户已经被检查过/将要检查的卡片已经在待删除列表里 跳过检查
-            if (userChecked.has(userId) || cardsToBeDeleted.has(card))  continue;
+            if (userChecked.has(userId) || cardsToBeDeleted.has(card)) continue;
             else {
                 try{
                     // console.log("知乎默认头像卡片移除 查询用户 " + userId);
@@ -46,14 +47,17 @@
                             .then(response => response.json())
                             .then(data => {
                                 // data.error: 大量请求知乎api后被反爬虫识别 需要到所给出的页面中进行真人验证
-                                if (data.error) {
+                                if (data.error){
+                                    alert("知乎默认头像卡片移除 需要进行真人验证，请在打开的窗口中完成！");
                                     window.open(data.error.redirect);
-                                } else {
+                                }
+                                else {
                                     // console.log("用户 " + userId + " 头像URL " + data.avatar_url_template);
-                                    if (data.avatar_url_template.toLowerCase().includes(DEFAULTAVATARHASH)) {
+                                    if (data.avatar_url_template.toLowerCase().includes(DEFAULTAVATARHASH)){
                                         console.log("待删除列表中加入：" + userId);
                                         cardsToBeDeleted.add(card);
-                                    } else {
+                                    } 
+                                    else{
                                         userChecked.add(userId.toLowerCase());
                                     }
                                 }
@@ -82,7 +86,6 @@
             // const cards = document.getElementsByClassName("Card TopstoryItem TopstoryItem-isRecommend");
 
             for (let card of cardsToBeDeleted){
-                // 每个内容卡片都具有class: "ContentItem ArticleItem"或"ContentItem AnswerItem"
                 let cardItem = card.querySelector("div.ContentItem");
                 // if (!cardItem) { cardItem = card.querySelector("div.ContentItem.ArticleItem"); }
 
@@ -94,7 +97,7 @@
                     }
                 }
 
-                console.log(`知乎默认头像卡片移除 已移除卡片: ${cardItem.getAttribute("data-zop")}, 原链接: ${JSON.stringify(urls)}, 预览: ${text}`);
+                console.log(`%c知乎默认头像卡片移除 已移除卡片: ${cardItem.getAttribute("data-zop")}, 原链接: ${JSON.stringify(urls)}, 预览: ${text}`, "color:#00A2E8");
                 card.remove();
             }
 
@@ -107,6 +110,8 @@
         console.log(`userChecked(${userChecked.size}): ` + JSON.stringify(Array.from(userChecked)));
     }
 
+    // 当检查到推荐流列表发生更新时的回调函数
+    // 在新增的卡片超过五个后即将新增的卡片传入findDefaultAvatarCard()对作者头像进行检查
     function checkIsNodeAddedCallback(mutationRecords, observer){
         const newAddedCards = new Set();
 
