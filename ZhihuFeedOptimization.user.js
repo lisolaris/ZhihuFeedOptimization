46732d697c333d22035d6439b97fe820a40c7acd
ligeek <ligeek@outlook.com>
2025-03-07 15:33:35 +0800

    Updated 0.3.6: regex filter, switch default avatar user ban, optimized error window

--- ZhihuFeedOptimization.user.js
+++ ZhihuFeedOptimization.user.js
@@ -7,7 +7,7 @@
 // @grant       GM_getValue
 // @grant       GM_registerMenuCommand
 // @grant       GM_unregisterMenuCommand
-// @version     0.3.5
+// @version     0.3.6
 // @run-at      document-idle
 // @author      lisolaris
 // @icon        https://www.google.com/s2/favicons?sz=64&domain=zhihu.com
@@ -24,34 +24,58 @@
     const DEFAULTAVATARHASH = "abed1a8c04700ba7d72b45195223e0ff";
     const DEFAULTAVATARHASHEXTENDS = new Set(["e1b6192aa0d8dcf140ba189dea518a4c", "1abe7b115ea0ab9e5dfe334d5a1fef38", "f1be5ed24936a7311e75da3884b2bd6d", "e5aeb4872c80f1b3edccc46617dfe3de", "10b20470e80a6274affe25aeba407dce", "5fc67a2efe2e8f52b40fac8a80da1442", "25ac7c0d6225fc37e7f4419d75895b22"]);
 
+    const RERE = /^\/.+\/$/
+
     const userChecked = new Set();
     const cardsToBeDeletedWithBannedWord = new Set();
     const cardsToBeDeletedWithDefaultAvatar = new Set();
 
     const bannedWords = new Set();
-    var bannedWordsJson = GM_getValue("bannedWords", "");
+    const bannedWordsRegex = new Set();
+
+    var bannedWordsJson = GM_getValue("bannedWords", "[]");
+    var bannedWordsRegexJson = GM_getValue("bannedWordsRegex", "[]");
+
+    var errorConfirmed = false;
+
     if (bannedWordsJson.length)
-        for (let word of JSON.parse(bannedWordsJson)) 
+        for (let word of JSON.parse(bannedWordsJson))
             bannedWords.add(word);
+    if (bannedWordsRegexJson.length)
+        for (let word of JSON.parse(bannedWordsRegexJson))
+            bannedWordsRegex.add(new RegExp(word.slice(1, -1)));
 
     var newCardsThreshold = parseInt(GM_getValue("newCardsThreshold", "5"));
     var answerCountThreshold = parseInt(GM_getValue("answerCountThreshold", "100"));
-    var usernameAuxJudgment = parseInt(GM_getValue("usernameAuxJudgment", "1"));
-    var autoSendUninterestWithBannedWordCard = parseInt(GM_getValue("autoSendUninterestWithBannedWordCard", "1"));
-    var useExtendAvatarDatabase = parseInt(GM_getValue("useExtendAvatarDatabase", "0"));
+    var enableDefaultAvatarFilter = parseInt(GM_getValue("defaultAvatarFilter", "1"));
+    var enableUsernameAuxJudgment = parseInt(GM_getValue("usernameAuxJudgment", "1"));
+    var enableAutoSendUninterestWithBannedWordCard = parseInt(GM_getValue("autoSendUninterestWithBannedWordCard", "1"));
+    var enableUseExtendAvatarDatabase = parseInt(GM_getValue("useExtendAvatarDatabase", "0"));
 
     async function checkIfBannedWordInCard(newCards){
         for (let card of newCards){
             // 等待 并心怀希望 卡片子元素的属性添加完毕后再做检查
             // for (let i=0; i<10 || !card.querySelector("div.ContentItem").hasAttribute("data-za-extra-module"); i++) console.log(`checkIfBannedWordInCard 等待${i}次`);
             // 检查标题是否含有用户屏蔽词
+            let cardTitle = card.querySelector("h2").textContent.toLowerCase();
+
+            for (let regex of bannedWordsRegex){
+                if (cardTitle.match(regex)){
+                    let cardItem = card.querySelector("div.ContentItem");
+                    let userId = JSON.parse(cardItem.getAttribute("data-za-extra-module")).card.content.author_member_hash_id;
+                    console.log(`%c知乎推荐流优化 待删除列表中加入: ${userId}, 原因: 用户屏蔽正则 ${regex}`, "color:#00A2E8");
+                    cardsToBeDeletedWithBannedWord.add(card);
+                    return;
+                }
+            }
+
             for (let word of bannedWords){
-                if (card.querySelector("h2").textContent.includes(word)){
+                if (cardTitle.includes(word)){
                     let cardItem = card.querySelector("div.ContentItem");
                     let userId = JSON.parse(cardItem.getAttribute("data-za-extra-module")).card.content.author_member_hash_id;
                     console.log(`%c知乎推荐流优化 待删除列表中加入: ${userId}, 原因: 用户屏蔽词 ${word}`, "color:#00A2E8");
                     cardsToBeDeletedWithBannedWord.add(card);
-                    break;
+                    return;
                 }
             }
         }
@@ -69,22 +93,27 @@
                     // data.error: 大量请求知乎api后被反爬虫识别 需要到所给出的页面中进行真人验证
                     if (data.error){
                         console.log(JSON.stringify(data));
-                        if (confirm("知乎推荐流优化 需要进行真人验证，请在打开的窗口中完成！"))
-                            window.open(data.error.redirect);
+                        if (!errorConfirmed){   // 只弹出一次错误信息窗口
+                            if (confirm(`知乎推荐流优化 请求后端接口时发生错误：${data.error.message}`)){
+                                errorConfirmed = true;
+                                if (data.error.redirect)
+                                    window.open(data.error.redirect);
+                            }
+                        }
                     }
                     else {
                         // console.log("用户 " + userId + " 头像URL " + data.avatar_url_template);
                         // 判定条件: 是默认头像 且 (回答数小于设定阈值 或 (用户是否启用 与 以“知乎用户”为用户名的开头))
                         // 真值表参见说明文档
                         userInfo["isDefaultAvatar"] = data.avatar_url_template.toLowerCase().includes(DEFAULTAVATARHASH);
-                        if (useExtendAvatarDatabase)
+                        if (enableUseExtendAvatarDatabase)
                             for (let hash of DEFAULTAVATARHASHEXTENDS)
                                 userInfo["isDefaultAvatar"] |= data.avatar_url_template.toLowerCase().includes(hash);
 
                         userInfo["isDefaultUsername"] = data.name.search(/^知乎用户/) == 0;
                         userInfo["answerCount"] = data.answer_count;
 
-                        if (userInfo["isDefaultAvatar"] && (userInfo["answerCount"] < answerCountThreshold || (usernameAuxJudgment && userInfo["isDefaultUsername"])))
+                        if (userInfo["isDefaultAvatar"] && (userInfo["answerCount"] < answerCountThreshold || (enableUsernameAuxJudgment && userInfo["isDefaultUsername"])))
                                 // if (!(cardType === "article" && data.favorited_count > 50)){
                                 userInfo["matched"] = true;
                     }
@@ -143,7 +172,7 @@
                         }
                     }
                     // 经测试不能直接通过xhr请求向知乎api发送不感兴趣 只能通过模拟点击来实现
-                    let isAutoSendUninterest = Boolean(cardSet === cardsToBeDeletedWithBannedWord && autoSendUninterestWithBannedWordCard);
+                    let isAutoSendUninterest = Boolean(cardSet === cardsToBeDeletedWithBannedWord && enableAutoSendUninterestWithBannedWordCard);
                     if (isAutoSendUninterest){
                         const floatLayerMenuObConfig = {attributes: false, childList: true, subtree: false};
                         const floatLayerMenuObserver = new MutationObserver(function(mutationRecords, observer){
@@ -181,7 +210,8 @@
         // for (let c of cards) console.log(c.querySelector('h2').innerText);
         console.log("知乎推荐流优化 检查新获得的推荐卡片列表……");
         checkIfBannedWordInCard(cards);
-        checkIfAuthorDefaultAvatarInCard(cards);
+        if (enableDefaultAvatarFilter)
+            checkIfAuthorDefaultAvatarInCard(cards);
     }
 
     // 当检查到推荐流列表发生更新时的回调函数
@@ -226,6 +256,7 @@
     (function (){
         var menuId_setNewCardsCountThreshold = null;
         var menuId_setAnswerCountThreshold = null;
+        var menuId_toggleDefaultAvatarFilter = null;
         var menuId_toggleUsernameAuxJudgment = null;
         var menuId_toggleAutoSendUninterest = null;
         var menuId_toggleUseExtendAvatarDatabase = null;
@@ -234,15 +265,35 @@
             let words = prompt("请输入屏蔽词，输入多个时以','分隔: ");
             // console.log("知乎推荐流优化 用户输入: " + words);
             if (words){
-                words = words.replaceAll(/\s*/g,"").replaceAll("，", ",");
+                words = words.replaceAll(/\s*/g, "").replaceAll("，", ",");    // \s: 匹配空白字符（空格、制表符、换行符）
 
                 let wordlist = words.split(",");
-                for (let w of wordlist) bannedWords.add(w);
+                let newRegexes = [];
+                let newWords = [];
+
+                for (let word of wordlist){
+                    if (word.match(RERE)){
+                        newRegexes.push(word);
+                        bannedWordsRegex.add(new RegExp(word.slice(1, -1)));
+                    }
+                    else{
+                        newWords.push(word);
+                        bannedWords.add(word);
+                    }
+                }
 
                 bannedWordsJson = JSON.stringify(Array.from(bannedWords));
-                console.log("知乎推荐流优化 用户屏蔽词库: " + bannedWordsJson);
+                bannedWordsRegexJson = JSON.stringify(Array.from(bannedWordsRegex).map(x => x.toString()));
                 GM_setValue("bannedWords", bannedWordsJson);
-                alert(`知乎推荐流优化 已添加屏蔽词: ${JSON.stringify(wordlist)} \n当前屏蔽词库: ${bannedWordsJson}`);
+                GM_setValue("bannedWordsRegex", bannedWordsRegexJson);
+
+                alert("知乎推荐流优化\n"
+                     + (newWords.length > 0 ? `已添加屏蔽词: ${JSON.stringify(newWords)}\n` : "")
+                     + (newRegexes.length > 0 ? `已添加正则表达式: ${JSON.stringify(newRegexes)}\n` : "")
+                     + `\n当前屏蔽词库: ${bannedWordsJson}\n`
+                     + `当前屏蔽正则库: ${bannedWordsRegexJson}\n`);
+                console.log("知乎推荐流优化 用户屏蔽词库: " + bannedWordsJson);
+                console.log("知乎推荐流优化 用户屏蔽正则库: " + bannedWordsRegexJson);
             }
         }
 
@@ -253,22 +304,47 @@
                 if (words === "清空全部屏蔽词！"){
                     if (confirm("知乎推荐流优化 确定要清空屏蔽词列表吗？")){
                         bannedWordsJson = "[]";
+                        bannedWordsRegexJson = "[]";
                         bannedWords.clear();
+                        bannedWordsRegex.clear();
                         GM_setValue("bannedWords", bannedWordsJson);
+                        GM_setValue("bannedWordsRegex", bannedWordsRegexJson);
                         alert("知乎推荐流优化 已清空屏蔽词列表");
-                        console.log("知乎推荐流优化 用户屏蔽词库: " + bannedWordsJson);
                     }
                 }
                 else{
                     words = words.replaceAll(/\s*/g,"").replaceAll("，", ",");
                     let wordlist = words.split(",");
-                    for (let w of wordlist) bannedWords.delete(w);
+                    let delWords = [];
+                    let delRegexes = [];
+                    for (let word of wordlist){
+                        if (word.match(RERE)){
+                            delRegexes.push(word);
+                        }
+                        else{
+                            delWords.push(word);
+                            bannedWords.delete(word);
+                        }
+                    }
 
-                    bannedWordsJson = JSON.stringify(Array.from(bannedWords));
-                    console.log("知乎推荐流优化 用户屏蔽词库: " + bannedWordsJson);
+                    if (delRegexes.length)
+                        for (let re of bannedWordsRegex)
+                            if (delRegexes.includes(re.toString()))
+                                bannedWordsRegex.delete(re)
+
+                    bannedWordsJson = JSON.stringify([...bannedWords])
+                    bannedWordsRegexJson = JSON.stringify([...bannedWordsRegex].map(x => x.toString()));
                     GM_setValue("bannedWords", bannedWordsJson);
-                    alert(`知乎推荐流优化 已删除屏蔽词: ${JSON.stringify(wordlist)} \n当前屏蔽词库: ${bannedWordsJson}`);
+                    GM_setValue("bannedWordsRegex", bannedWordsRegexJson);
+
+                    alert("知乎推荐流优化\n"
+                        + (delWords.length > 0 ? `已删除屏蔽词: ${JSON.stringify(delWords)}\n` : "")
+                        + (delRegexes.length > 0 ? `已删除正则表达式: ${JSON.stringify(delRegexes)}\n` : "")
+                        + `\n当前屏蔽词库: ${bannedWordsJson}\n`
+                        + `当前屏蔽正则库: ${bannedWordsRegexJson}\n`);
                 }
+                console.log("知乎推荐流优化 用户屏蔽词库: " + bannedWordsJson);
+                console.log("知乎推荐流优化 用户屏蔽正则库: " + bannedWordsRegexJson);
             }
         }
 
@@ -290,21 +366,27 @@
             }
         }
 
+        function menuToggleDefaultAvatarFilter(){
+            enableDefaultAvatarFilter = !enableDefaultAvatarFilter;
+            GM_setValue("defaultAvatarFilter", (enableDefaultAvatarFilter ? 1 : 0));
+            menuUpdateVariableMenuInOrder();
+        }
+
         function menuToggleUsernameAuxJudgment(){
-            usernameAuxJudgment = !usernameAuxJudgment;
-            GM_setValue("usernameAuxJudgment", (usernameAuxJudgment ? 1 : 0));
+            enableUsernameAuxJudgment = !enableUsernameAuxJudgment;
+            GM_setValue("usernameAuxJudgment", (enableUsernameAuxJudgment ? 1 : 0));
             menuUpdateVariableMenuInOrder();
         }
 
         function menuToggleAutoSendUninterest(){
-            autoSendUninterestWithBannedWordCard = !autoSendUninterestWithBannedWordCard;
-            GM_setValue("autoSendUninterestWithBannedWordCard", (autoSendUninterestWithBannedWordCard ? 1 : 0));
+            enableAutoSendUninterestWithBannedWordCard = !enableAutoSendUninterestWithBannedWordCard;
+            GM_setValue("autoSendUninterestWithBannedWordCard", (enableAutoSendUninterestWithBannedWordCard ? 1 : 0));
             menuUpdateVariableMenuInOrder();
         }
 
         function menuToggleUseExtendAvatarDatabase(){
-            useExtendAvatarDatabase = !useExtendAvatarDatabase;
-            GM_setValue("useExtendAvatarDatabase", (useExtendAvatarDatabase ? 1 : 0));
+            enableUseExtendAvatarDatabase = !enableUseExtendAvatarDatabase;
+            GM_setValue("useExtendAvatarDatabase", (enableUseExtendAvatarDatabase ? 1 : 0));
             menuUpdateVariableMenuInOrder();
         }
 
@@ -312,28 +394,32 @@
         function menuUpdateVariableMenuInOrder(){
             GM_unregisterMenuCommand(menuId_setAnswerCountThreshold);
             GM_unregisterMenuCommand(menuId_setNewCardsCountThreshold);
+            GM_unregisterMenuCommand(menuId_toggleDefaultAvatarFilter);
             GM_unregisterMenuCommand(menuId_toggleUsernameAuxJudgment);
             GM_unregisterMenuCommand(menuId_toggleAutoSendUninterest);
             GM_unregisterMenuCommand(menuId_toggleUseExtendAvatarDatabase);
 
             menuId_setAnswerCountThreshold = GM_registerMenuCommand(`设置答案数量阈值（${answerCountThreshold}）`, menuSetAnswerCountThreshold);
             menuId_setNewCardsCountThreshold = GM_registerMenuCommand(`设置新卡片数量阈值（${newCardsThreshold}）`, menuSetNewCardsCountThreshold);
-            menuId_toggleUsernameAuxJudgment = GM_registerMenuCommand(`切换用户名辅助判定（${usernameAuxJudgment ? "是" : "否"}）`, menuToggleUsernameAuxJudgment, {autoClose: false});
-            menuId_toggleAutoSendUninterest = GM_registerMenuCommand(`切换自动点击不感兴趣（${autoSendUninterestWithBannedWordCard ? "是" : "否"}）`, menuToggleAutoSendUninterest, {autoClose: false});
-            menuId_toggleUseExtendAvatarDatabase = GM_registerMenuCommand(`切换使用扩展默认头像库（${useExtendAvatarDatabase ? "是" : "否"}）`, menuToggleUseExtendAvatarDatabase, {autoClose: false});
+            menuId_toggleDefaultAvatarFilter = GM_registerMenuCommand(`是否启用默认头像屏蔽（${enableDefaultAvatarFilter ? "是" : "否"}）`, menuToggleDefaultAvatarFilter, {autoClose: false});
+            if (enableDefaultAvatarFilter)  // 在启用了屏蔽默认头像后才会显示用户名辅助判定菜单
+                menuId_toggleUsernameAuxJudgment = GM_registerMenuCommand(`是否启用用户名辅助判定（${enableUsernameAuxJudgment ? "是" : "否"}）`, menuToggleUsernameAuxJudgment, {autoClose: false});
+            menuId_toggleAutoSendUninterest = GM_registerMenuCommand(`是否自动点击不感兴趣（${enableAutoSendUninterestWithBannedWordCard ? "是" : "否"}）`, menuToggleAutoSendUninterest, {autoClose: false});
+            menuId_toggleUseExtendAvatarDatabase = GM_registerMenuCommand(`是否使用扩展默认头像库（${enableUseExtendAvatarDatabase ? "是" : "否"}）`, menuToggleUseExtendAvatarDatabase, {autoClose: false});
         }
 
         GM_registerMenuCommand("添加屏蔽词", menuAddBannedWords);
         GM_registerMenuCommand("删除屏蔽词", menuRemoveBannedWords);
         menuUpdateVariableMenuInOrder();
 
-        console.log("知乎推荐流优化 已完成加载");
-        console.log("知乎推荐流优化 用户屏蔽词库: " + JSON.parse(bannedWordsJson).join(","));
+        console.log("知乎推荐流优化 用户屏蔽词库: " + JSON.parse(bannedWordsJson).join(", "));
+        console.log("知乎推荐流优化 用户屏蔽正则库: " + JSON.parse(bannedWordsRegexJson).join(", "));
         console.log("知乎推荐流优化 答案数量阈值: " + answerCountThreshold);
         console.log("知乎推荐流优化 新卡片数量阈值: " + newCardsThreshold);
-        console.log("知乎推荐流优化 是否使用用户名作为辅助判断: " + (usernameAuxJudgment ? "是" : "否"));
-        console.log("知乎推荐流优化 是否自动对匹配屏蔽词的卡片点击不感兴趣: " + (autoSendUninterestWithBannedWordCard ? "是" : "否"));
-        console.log("知乎推荐流优化 是否使用扩展默认头像库: " + (useExtendAvatarDatabase ? "是" : "否"));
+        console.log("知乎推荐流优化 是否启用默认头像屏蔽: " + (enableDefaultAvatarFilter ? "是" : "否"));
+        console.log("知乎推荐流优化 是否使用用户名作为辅助判断: " + (enableUsernameAuxJudgment ? "是" : "否"));
+        console.log("知乎推荐流优化 是否自动对匹配屏蔽词的卡片点击不感兴趣: " + (enableAutoSendUninterestWithBannedWordCard ? "是" : "否"));
+        console.log("知乎推荐流优化 是否使用扩展默认头像库: " + (enableUseExtendAvatarDatabase ? "是" : "否"));
     })();
 
     const recomBody = document.querySelector("div.Topstory-recommend");
@@ -349,7 +435,7 @@
     //     console.log(`userChecked(${userChecked.size}): ` + JSON.stringify(Array.from(userChecked)));
     // },
     // 5000);
-    console.log("知乎推荐流优化v0.3.5 已加载完成");
+    console.log(`知乎推荐流优化v${GM.info.script.version} 已加载完成`);
 
     // 首次加载时间较长 等待后再做检查
     await sleep(2000);
